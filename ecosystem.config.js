de5abbfa96abb504410ea2f366f316003478bc86
wolfdong7 <wolfdong7@qq.com>
2017-10-06 22:11:25 +0800

    zzz

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,21 +7,15 @@ module.exports = {
 
     // First application
     {
-      name      : 'API',
-      script    : 'app.js',
+      name      : 'node_bushu',
+      script    : 'server.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
       env_production : {
         NODE_ENV: 'production'
       }
-    },
-
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
-    }
+    }    
   ],
 
   /**
@@ -30,19 +24,19 @@ module.exports = {
    */
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'root',
+      host : '192.168.1.117',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
+      repo : 'https://github.com/wolfdong7/node_bushu',
+      path : '/var/www/node_bushu/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'root',
+      host : '192.168.1.117',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
+      repo : 'https://github.com/wolfdong7/node_bushu',
+      path : '/var/www/node_bushu/development',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
       env  : {
         NODE_ENV: 'dev'
